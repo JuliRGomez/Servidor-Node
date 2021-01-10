fda7bcf344a56ed43cc65ab797c657fe374bad0d
Juli R <julirg58@gmail.com>
2021-01-10 16:26:10 -0500

    etrega finalizada funcionando

--- index.js
+++ index.js
@@ -2,18 +2,8 @@ let http = require("http");
 let fs = require("fs");
 let path= require("path"); 
 let mime = require("mime");
+const reciveData=["firstName","lastName","Email","Password"];
 
-
-function read(url,response){
-    const urlF=__dirname+url;
-    fs.readFile(urlF,(error,content)=>{
-        !error?(setContentType(urlF,response),
-                response.write(content),
-                response.end()):(
-        response.write("<h1>404 </h1>"),
-        response.end());
-    })
-}
 
 http.createServer((request,response)=>{
     if(request.method === "GET"){
@@ -22,64 +12,41 @@ http.createServer((request,response)=>{
                 read("/index.html",response)
                 break;
             case "/nosotros":
-                read("./about.html",response)
+                read("/about.html",response)
                 break;
             case "/proyectos":
-                read("./projects.html",response)
+                read("/projects.html",response)
                 break;
             case "/contacto":
-                read("./contact.html",response)
+                read("/contact.html",response)
                 break;
             case "/favicon.ico":
                 response.setHeader("Content-Type","image/x-icon");
                 read("./favicon.ico",response)
                 break;
             default:
-                response.statusCode=404;
-                read("./404.html",response)
+                read(request.url,response)
                 break;
         }
     }
     else if(request.method === "POST"){
         
-        let previousData="";
         let data = '';
         
+
         request.on('data', chunk => {
-             //console.log("entrando");
-            //console.log(chunk);
             data += chunk;
         });
         request.on('end', () => {
-            //data=decodeURIComponent(escape(data))
-        
-          narray=data.split(" ");
-          console.log(narray[0]);     
-            let dataSplit=data.split("&");
-            let completeString=`Nombre: ${dataSplit[0]}
-Apellidos: ${dataSplit[1]}
-Email: ${dataSplit[2]}
-Contraseña: ${dataSplit[3]}`
-            //console.log(completeString);
-            //console.log(data.toString());
-            //console.log("Fin del stream")
-            fs.readFile("db_usuarios.txt",(error,content)=>{
-                if(!error){
-                    previousData=content;
-                }
-            })
-            fs.writeFile("db_usuarios.txt",previousData+completeString, (error) => {
-                if(error){
-                    console.log(error);
-                }
-                else{
-                   // response.setHeader("Content-Type","text/html");
-                  //  response.write("<h1>Datos guardado</h1>")
-                  response.setHeader("location","/");//redireccion hacia raiz
-                  response.statusCode=302;//este debe ser el codigo de redirecion
-                  response.end();
-                }
+            
+            let userobj={firstName:"",lastName:"",Email:"",Password:""};
+            reciveData.forEach(element=>{
+                let pos=data.search(element);
+                let pos2=data.indexOf("-",pos);
+                userobj[element]=data.slice(pos+element.length+1,pos2).trim();
             });
+            console.log(userobj);
+            dataSave("db_usuarios.json",userobj,response)
         });
 
         request.on('error', error => {
@@ -90,16 +57,49 @@ Contraseña: ${dataSplit[3]}`
 
 }).listen(8080);//los servideros web usan regularmente el 8080 o 8000
 
-const setContentType = (address,response)=>{
-    const ext=path.extname(address)
-    switch (ext) {
-        case ".css":
-            response.setHeader("Content-Type","text/css;charset=utf-8");
-            break;
-        case ".html":
-            response.setHeader("Content-Type","text/html;charset=utf-8");
-            break;
-        default:
-            break;
-    }
+const dataSave =(db,user,response)=>{
+    let previousData;
+    fs.readFile(db,(error,content)=>{
+        if(!error){
+            previousData=JSON.parse(content);
+            previousData.push(user);
+            fs.writeFile(db,JSON.stringify(previousData), (error) => {
+                if(error){
+                    console.log(error);
+                }
+                else{
+                response.setHeader("location","/");//redireccion hacia raiz
+                response.statusCode=302;//este debe ser el codigo de redirecion
+               
+                }
+            });
+            }
+    })
 }
+
+function read(url,response){
+    const urlF=__dirname+url;
+    fs.readFile(urlF,(error,content)=>{
+        !error?(
+                response.setHeader("Content-Type", mime.getType(urlF)),
+                response.write(content),
+                response.end()):(
+                response.statusCode=404,
+                response.write("<h1>404 </h1>"),
+                    response.end());
+    })
+}
+
+// const setContentType = (address,response)=>{
+//     const ext=path.extname(address)
+//     switch (ext) {
+//         case ".css":
+//             response.setHeader("Content-Type","text/css;charset=utf-8");
+//             break;
+//         case ".html":
+//             response.setHeader("Content-Type","text/html;charset=utf-8");
+//             break;
+//         default:
+//             break;
+//     }
+// }
